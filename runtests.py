91c83b58b35728618583f13403284a61d108bcb7
Martin Sandström <martin@marteinn.se>
2015-05-07 22:53:37 +0200

    Updated unittest runner (we use the same as frojd_fabric)

--- runtests.py
+++ runtests.py
@@ -1,6 +1,36 @@
+import sys
 import unittest
 from tests import *
 
 
+test_suite = [
+    "tests"
+]
+
+
+def runtests(test_modules=None):
+    # List of modules to test
+    if (not test_modules):
+        test_modules = test_suite
+
+    # Construct and run test suite
+    suite = unittest.TestSuite()
+
+    for t in test_modules:
+        try:
+            mod = __import__(t, globals(), locals(), ["suite"])
+            suitefn = getattr(mod, "suite")
+            suite.addTest(suitefn())
+        except (ImportError, AttributeError):
+            suite.addTest(unittest.defaultTestLoader.loadTestsFromName(t))
+
+    unittest.TextTestRunner().run(suite)
+
+
 if __name__ == "__main__":
-    unittest.main()
+    test_modules = None
+
+    if len(sys.argv) > 1:
+        test_modules = [sys.argv[1]]
+
+    runtests(test_modules)
