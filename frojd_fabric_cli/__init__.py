f4de12982fa852e827d5273649d64b0278c83e7f
Martin Sandström <martin@marteinn.se>
2015-06-04 21:33:31 +0200

    Updated version

--- frojd_fabric_cli/__init__.py
+++ frojd_fabric_cli/__init__.py
@@ -10,7 +10,7 @@ frojd_fabric_cli
 
 
 __title__ = "frojd_fabric_cli"
-__version__ = "1.0.4"
-__build__ = 104
+__version__ = "1.0.0"
+__build__ = 1
 __license__ = "MIT"
 __copyright__ = "Copyright 2015 Fröjd Interactive AB"
