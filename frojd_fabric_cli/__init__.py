4d2f1e415b3f95e6495af5fe5a29764a0d5c9fba
Martin Sandström <martin@marteinn.se>
2015-09-02 21:12:32 +0200

    Bumped version

--- frojd_fabric_cli/__init__.py
+++ frojd_fabric_cli/__init__.py
@@ -10,7 +10,7 @@ fabrik_cli
 
 
 __title__ = "fabrik_cli"
-__version__ = "1.1.1"
-__build__ = 111
+__version__ = "1.1.2"
+__build__ = 112
 __license__ = "MIT"
 __copyright__ = "Copyright 2015 Fröjd Interactive AB"
