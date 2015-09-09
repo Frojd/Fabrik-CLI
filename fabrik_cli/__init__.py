547c5504dcbe594bb760c6c4530cb93dc86f8406
Martin Sandström <martin@marteinn.se>
2015-09-09 12:21:29 +0200

    Bumped version

--- fabrik_cli/__init__.py
+++ fabrik_cli/__init__.py
@@ -10,7 +10,7 @@ fabrik_cli
 
 
 __title__ = "fabrik_cli"
-__version__ = "1.1.2"
-__build__ = 112
+__version__ = "2.0.0"
+__build__ = 200
 __license__ = "MIT"
 __copyright__ = "Copyright 2015 Fröjd Interactive AB"
